6b6651b6c62f864be9daa226993b5109b950fe45
Ejiro-Frances <francesejiro@yahoo.com>
2024-12-08 03:47:37 -0800

    updated animation

--- js/script.js
+++ js/script.js
@@ -63,3 +63,23 @@ serviceLinks.forEach((li) => {
 main.addEventListener("click", () => {
   serviceContainer.classList.remove("active");
 });
+
+// scrollers
+const scrollers = document.querySelectorAll(".scroller");
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
